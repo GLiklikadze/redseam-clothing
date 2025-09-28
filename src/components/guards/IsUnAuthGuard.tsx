7a790a24af95b3ec537b19b7e9204cdebcc2ecbe
GLiklikadze <102631687+GLiklikadze@users.noreply.github.com>
2025-09-28 20:07:56 +0400

    add logic to isUnauthGuard

--- src/components/guards/IsUnAuthGuard.tsx
+++ src/components/guards/IsUnAuthGuard.tsx
@@ -1,10 +1,12 @@
+import { useGetCartData } from "@/react-query/query/cart/cartQuery";
 import { PropsWithChildren } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const IsUnAuthGuard: React.FC<PropsWithChildren> = ({ children }) => {
   const token = localStorage.getItem("auth_token");
+  const { data: listData } = useGetCartData();
 
-  if (!token) {
+  if (!token || (listData?.length ?? 0) < 1) {
     return <Navigate to="/" />;
   }
 
