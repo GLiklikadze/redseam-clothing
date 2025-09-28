e5307e6bdc32dcd797254f1f37bbb3cda7466f98
GLiklikadze <102631687+GLiklikadze@users.noreply.github.com>
2025-09-28 13:23:55 +0400

    vercel error finding 3

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,4 @@
-import tailwindPlugin from "prettier-plugin-tailwindcss";
+import * as tailwindPlugin from "prettier-plugin-tailwindcss";
 
 /** @type {import("prettier").Config} */
 export default {
